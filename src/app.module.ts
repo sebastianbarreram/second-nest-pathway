c984f5ae2bc428ddebe2c28a44553f575da186e9
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2022-11-09 10:15:10 -0500

    feat: CRUD Customer and CRUD Invoice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './main/app.controller';
+import { AppService } from './main/app.service';
+import { CustomerModule } from './customer/customer.module';
+import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [],
+  imports: [CustomerModule, InvoiceModule],
   controllers: [AppController],
   providers: [AppService],
 })
